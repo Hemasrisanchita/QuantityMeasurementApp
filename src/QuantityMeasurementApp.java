0c1dced84b2e5939238319d85112863485ed93d2
Hemasrisanchita <sanchitha112006@gmail.com>
2026-04-17 16:22:44 +0530

    UC2: Inches measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -15,7 +15,21 @@ public class QuantityMeasurementApp {
             return Double.compare(feet.value, value) == 0;
         }
     }
+    static class Inches {
+        private final double value;
 
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+    }
     public static void main(String[] args) {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
