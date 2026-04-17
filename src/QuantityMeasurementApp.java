d4e4337169835bd56a0ac941968172833fb87bd9
Hemasrisanchita <sanchitha112006@gmail.com>
2026-04-17 16:28:59 +0530

    UC3: Generic Quantity class with unit conversion

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,38 +1,44 @@
 public class QuantityMeasurementApp {
 
-    static class Feet {
-        private final double value;
+    enum LengthUnit {
+        FEET(1.0),
+        INCHES(1.0 / 12.0);
 
-        public Feet(double value) {
-            this.value = value;
+        private final double toFeet;
+
+        LengthUnit(double toFeet) {
+            this.toFeet = toFeet;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, value) == 0;
+        public double toFeet(double value) {
+            return value * toFeet;
         }
     }
-    static class Inches {
-        private final double value;
 
-        public Inches(double value) {
+    static class Quantity {
+        private final double value;
+        private final LengthUnit unit;
+
+        public Quantity(double value, LengthUnit unit) {
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
-            Inches inches = (Inches) obj;
-            return Double.compare(inches.value, value) == 0;
+            Quantity q = (Quantity) obj;
+            return Double.compare(this.toFeet(), q.toFeet()) == 0;
         }
     }
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        Quantity f1 = new Quantity(1.0, LengthUnit.FEET);
+        Quantity f2 = new Quantity(1.0, LengthUnit.INCHES);
 
         System.out.println("Equal: " + f1.equals(f2));
     }
